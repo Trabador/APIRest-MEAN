5a30eb11a1153969910d15498fc14a41423e16cf
Alexis Garcia <alexclaustro@gmail.com>
2017-11-21 01:09:13 -0600

    Add post function in agregar button.Add update function in actulizar button.

--- public/client/controller.js
+++ public/client/controller.js
@@ -2,6 +2,8 @@ var app = angular.module("API",[]);
 
 app.controller("generalCtrl", function($scope, $http){
     $scope.products = [];
+    $scope.product = null;
+    $scope.action = 'Agregar';
 
     var req = {
         method: 'GET',
@@ -25,7 +27,7 @@ app.controller("generalCtrl", function($scope, $http){
         };
 
         $http(req)
-            .then(function succes (res){
+            .then(function success (res){
                 console.log(res);
                 //reload page
                 window.location.reload();
@@ -34,6 +36,64 @@ app.controller("generalCtrl", function($scope, $http){
                 console.log(res);
             }
         );
+    };
+
+    $scope.sendData = function(){
+        if ($scope.product._id == null){
+            var url = 'http://localhost:8080/api/Product/';
+            addProduct(url);
+        }
+        else{
+            var url = 'http://localhost:8080/api/Product/'+$scope.product._id;
+            updateProduct(url);
+        }    
+    };
+
+    $scope.updateThisProduct = function(id){
+        var url = 'http://localhost:8080/api/Product/'+id;
+        $http.get(url)
+            .then(function success (res){
+                    console.log(res);
+                    $scope.action = 'Actualizar';
+                    $scope.product = res.data.message;
+                },
+                function error (res){
+                    console.log(res);
+                    $scope.product = null;
+                }
+        );
+    };
+
+    function addProduct(url){
+        console.log($scope.product);
+        $http.post(url, $scope.product)
+            .then(function success (res){
+                console.log(res);
+                //reload page
+                window.location.reload();
+            },
+            function error (res){
+                console.log(res);
+            }
+        );
+        $scope.product = {};
+    };
+
+    function updateProduct(url){
+        console.log($scope.product);
+        $http.put(url, $scope.product)
+            .then(function success (res){
+                console.log(res);
+                //reload page
+                $scope.action = 'Agregar';
+                window.location.reload();
+            },
+            function error (res){
+                console.log(res);
+            }
+        );
+        $scope.product = {};
+
     };
 
 });
