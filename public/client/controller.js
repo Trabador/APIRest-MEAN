cf39b2e6315bbc0cb7e98a6c603c4bece715a3e9
Alexis Garcia <alexclaustro@gmail.com>
2017-11-20 20:16:27 -0600

    Add boostrap, create basic page.Add load all products in page.Add delete function from button

--- public/client/controller.js
+++ public/client/controller.js
@@ -3,9 +3,37 @@ var app = angular.module("API",[]);
 app.controller("generalCtrl", function($scope, $http){
     $scope.products = [];
 
-    $http.get('http://localhost:8080/api/Product/')
-        .then(function(res){
-            $scope.products = res.data.message;
-        })
-        .catch();
+    var req = {
+        method: 'GET',
+        url: 'http://localhost:8080/api/Product/'
+    };
+
+    $http(req)
+        .then(function succes (res){
+                $scope.products = res.data.message;
+            },
+            function error (res){
+                console.log(res);
+                $scope.products = [];
+            }
+        );
+
+    $scope.deleteThisProduct = function(id){
+        var req = {
+            method: 'DELETE',
+            url: 'http://localhost:8080/api/Product/'+id
+        };
+
+        $http(req)
+            .then(function succes (res){
+                console.log(res);
+                //reload page
+                window.location.reload();
+            },
+            function error (res){
+                console.log(res);
+            }
+        );
+    };
+
 });
